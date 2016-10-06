09ad785a90a39c75fc92bcc5c46a91293035b492
Guillaume Merindol <guillaume@merindol.co>
2016-10-06 17:12:51 -0400

    Tests

---
+++ countries-list_test.go
@@ -0,0 +1,20 @@
+package countries
+
+import "testing"
+
+func TestCountryList(t *testing.T) {
+	list := CountryList()
+
+	if list == nil || len(list) == 0 {
+		t.Error("List is empty?")
+	}
+
+	for k, v := range list {
+		if v.Alpha3 == "" || v.FullName == "" || v.Numeric == 0 {
+			t.Errorf("Missing information with country `%s'", k)
+		}
+		if len(k) != 2 || len(v.Alpha3) != 3 {
+			t.Errorf("Problem with the length of Alpha(s): %s & %s", k, v.Alpha3)
+		}
+	}
+}
---
+++ countries_test.go
@@ -0,0 +1,12 @@
+package countries
+
+import "testing"
+
+func TestVerify(t *testing.T) {
+	if ok := Alpha2("AF").Verify(); !ok {
+		t.Error("Should have been ok")
+	}
+	if ok := Alpha2("AA").Verify(); ok {
+		t.Error("Shouldn't have been ok")
+	}
+}
